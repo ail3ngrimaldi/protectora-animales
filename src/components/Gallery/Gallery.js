8d267c943958fc7be8fa7918c2c2b488f7c4c4fa
Acarvajal905 <acarvajal905@gmail.com>
2020-10-08 20:53:49 -0300

    .

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,18 +1,49 @@
 import React from "react";
-// import styles from './Gallery.css'
+import styles from './Gallery.css'
 import Info from "../Info/Info";
 
 export const Gallery = () => {
-    return (
-        <div>
-            <div className="row">
-                <div className="col-md-12 d-flex justify-content-center mb-5">
-                    <button type="button" className="btn btn-outline-black waves-effect filter" data-rel="all">Todos</button>
-                    <button type="button" className="btn btn-outline-black waves-effect filter" data-rel="1">Perros</button>
-                    <button type="button" className="btn btn-outline-black waves-effect filter" data-rel="2">Gatos</button>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12 d-flex justify-content-center mb-5">
+          <button type="button" className="btn btn-outline-black waves-effect filter" data-rel="all">Todos</button>
+          <button type="button" className="btn btn-outline-black waves-effect filter" data-rel="1">Perros</button>
+          <button type="button" className="btn btn-outline-black waves-effect filter" data-rel="2">Gatos</button>
+        </div>
+      </div>
       <div className="gallery" id="gallery">
+        <div className="mb-3 pics animation all 1">
+          <div className="imageInfo">
+            <img
+              className="img-fluid"
+              src="/img/Gallery/Dogs/Dogo1.jpg"
+              alt="Card cap"
+            ></img>
+            <div className="memberInfo">
+              <h2 className="mb-2 font-weight-light h3 ">
+                <Info />
+              </h2>
+              <span className="d-block mb-2 text-white-opacity-05 h4"></span>
+            </div>
+          </div>
+        </div>
+        <div className="mb-3 pics animation all 1">
+          <div className="imageInfo">
+            <img
+              className="img-fluid"
+              src="/img/Gallery/Dogs/Dogo1.jpg"
+              alt="Card cap"
+            ></img>
+            <div className="memberInfo">
+              <h2 className="mb-2 font-weight-light h3 ">
+                <Info />
+              </h2>
+              <span className="d-block mb-2 text-white-opacity-05 h4"></span>
+            </div>
+          </div>
+        </div>
+
         <div className="mb-3 pics animation all 1">
           <div className="imageInfo">
             <img
