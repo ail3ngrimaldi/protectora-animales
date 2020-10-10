8ad9b2958e54d9263f1ff79e8b707fe1eb55ffd2
queteimporta190 <darioezequiel.nunez@gmail.com>
2020-10-10 17:31:26 -0300

    Agregando el estilo que faltaba

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,40 @@
 import React from "react";
-//import { AboutUs } from "../AboutUs/AboutUs";
 import { Carrousel } from "../Carrousel/Carrousel";
-//import { History } from "../History/History";
+import { EventsHome } from "../Events/EventsHome";
+import { Link } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
+
+import "./Home.css";
 
 export const Home = () => {
   return (
-    <div>
-      <br />
-      <br />
+    <div className="StyleHome">
       <Carrousel />
+      <div>
+        <div className="container">
+          <div className="row">
+            <div
+              className="col-md-6 white-text text-center text-md-left mt-xl-5 mb-5 wow fadeInLeft"
+              data-wow-delay="0.3s"
+            >
+              <h1 className="h1-responsive font-weight-bold mt-sm-5">
+                PROTECTORA DE ANIMALES DOMINGO FAUSTINO SARMIENTO{" "}
+              </h1>
+              <div className="col-md-12 d-flex justify-content-left mb-5">
+                <Link to="/AboutUs">
+                  <Button>¡Conocenos!</Button>
+                </Link>
+              </div>
+            </div>
+            <img
+              className="StyleImg"
+              src="/img/Gallery/Dogs/Dogo4.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <EventsHome />
     </div>
   );
 };
