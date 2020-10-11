f95ca81956bd2fd85e1f5035ee3b6608ef94eb4b
Miguel Angel Jaimes <66225919+MrFoxxz@users.noreply.github.com>
2020-10-11 13:56:51 -0500

    DropBar y base de complementos Donations

--- src/components/Donations/Donations.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Donations = () => {
-    return (
-        <div>
-            <h3>Donations Screen</h3>
-        </div>
-    )
-}
---
+++ src/components/Donations/SuppliesDonations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SuppliesDonations = () => {
+    return (
+        <div>
+            <h3>SuppliesDonations Screen</h3>
+        </div>
+    )
+}
