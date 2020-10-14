351fccb14333c72c3f9652cc9b4a4a00bbd89d88
Miguel Angel Jaimes <66225919+MrFoxxz@users.noreply.github.com>
2020-10-14 12:50:30 -0500

    Estilos Donation y avanse en componente WhatsApp

--- src/components/Donations/SuppliesBanner.js
+++ src/components/Donations/SuppliesBanner.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
+import "./SuppliesBanner.css"
 
 export const SuppliesBanner = () => {
     return (
         <div>
-            <NavLink
-                exact
-                activeClassName="active"
-                to="/SuppliesDonations"
-                className="nav-item nav-link"
-            >
-                {" "}
-                Dona{" "}
-
-            </NavLink>
-
+            <div className="SBannerContainer">
+                <img
+                        class="Sbannerimg"
+                        src="/img/Captura.PNG"
+                        alt="Card cap"
+                        
+                ></img>
+                <div className="SBannerCard">
+                    <h4 class="card-title">Donativo Insumos</h4>
+                    <h5 class="card-title">Informacion</h5>
+                    <h5 class="card-title">Mensaje lindo</h5>
+                    <h6 class="card-title">Agradecimientos</h6>
+                 
+                    <NavLink
+                        exact
+                        activeClassName="active"
+                        to="/SuppliesDonations"
+                        className="nav-item nav-link">
+                        <button type="button" class="btn btn-primary">Dona aqui!</button>
+                    </NavLink>
+                </div>
+            </div>
         </div>
     )
 }
