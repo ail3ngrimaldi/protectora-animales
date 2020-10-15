d25292db75562ddc23abbbd1e840cc1ef6d7eaf1
hugotata78 <carlitosyagus_@hotmail.com.ar>
2020-10-15 16:07:41 -0300

    Perfil de usuario

---
+++ src/components/Users/UserProfile.js
@@ -0,0 +1,13 @@
+import React from react;
+import { connect } from 'react-redux'
+
+const UserProfile = (props) => {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default UserProfile
