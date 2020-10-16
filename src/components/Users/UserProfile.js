a44650e93a4e8e04b072db17238c070b186f6fc3
queteimporta190 <darioezequiel.nunez@gmail.com>
2020-10-16 14:06:07 -0300

    Estilo Formulario Usuario

--- src/components/Users/UserProfile.js
+++ src/components/Users/UserProfile.js
@@ -1,28 +1,76 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 const UserProfile = (props) => {
-    const {auth, profile} = props
-    //console.log(auth)
-    console.log(profile)
-    //const name = profile.hasOwnProperty('displayName') ? profile.displayName : profile.firstName + ' '+profile.lastName
-    return(
+    const { auth, profile } = props
+
+    return (
         <div>
             <h1>Mi Perfil</h1>
             {
-                profile.hasOwnProperty('displayName') ?
-                <p>{profile.displayName}</p>
-                :
-                <div>
-                    <p>Nombre: {profile.firstName}</p>
-                    <p>Apellido: {profile.lastName}</p>
-                    <p>Edad: {profile.age}</p>
-                    <p>Fecha de Nacimiento: {profile.birthdate}</p>
-                    <p>Localidad: {profile.location}</p>
-                    <p>Direccion: {profile.address}</p>
-                    <Link to='/EditarPerfil'><button >Modificar Perfil</button></Link>
-                </div>
+            profile.hasOwnProperty('displayName') ?
+                    <p>{profile.displayName}</p>
+                    :
+                    <div>
+                    <Container>
+                    <fieldset disabled> 
+                    <Form.Row >
+                    <Col xs="auto">
+                    <Form.Group  controlId="formGridNombre">
+                      <Form.Label>Nombre</Form.Label>
+                      <Form.Control type="email" placeholder={profile.firstName} />                      
+                    </Form.Group>
+                    </Col>
+
+                    <Form.Group controlId="formGridApellido">
+                      <Form.Label>Apellido</Form.Label>
+                      <Form.Control type="Apellido" placeholder={profile.lastName} />                      
+                    </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row >
+                    <Col xs="auto">
+                    <Form.Group controlId="formGridEdad">
+                      <Form.Label>Edad</Form.Label>
+                      <Form.Control type="Edad" placeholder={profile.age} />
+                    </Form.Group> 
+                    </Col> 
+
+                    <Col xs="auto">
+                    <Form.Group controlId="formBasicFNacimiento">
+                      <Form.Label>Fecha de Nacimiento</Form.Label>
+                      <Form.Control type="Fecha de Nacimiento" placeholder={profile.birthdate} />
+                    </Form.Group> 
+                    </Col>
+
+                    <Col xl="auto">
+                    <Form.Group controlId="formBasicDierccion">
+                      <Form.Label>Dirección</Form.Label>
+                      <Form.Control type="Dirección" placeholder={profile.address} />
+                    </Form.Group>
+                    </Col>
+
+                    <Col xs="auto">
+                    <Form.Group controlId="formBasicUbicacion">
+                      <Form.Label>Ubicación</Form.Label>
+                      <Form.Control type="Ubicación" placeholder={profile.location} />
+                    </Form.Group> 
+                    </Col>
+                    </Form.Row> 
+                    </fieldset>
+                    <Link to='/EditarPerfil'>
+                    <Button variant="primary" type="submit">
+                      Editar Perfil
+                    </Button>
+                    </Link>                    
+                  </Container>                  
+                  </div>
             }
         </div>
     )
@@ -30,10 +78,10 @@ const UserProfile = (props) => {
 
 const mapStateToProps = (state) => {
     // console.log(state);
-    return{
-      auth: state.firebase.auth,
-      profile: state.firebase.profile
+    return {
+        auth: state.firebase.auth,
+        profile: state.firebase.profile
     }
-  }
-  
-  export default connect(mapStateToProps)(UserProfile)
+}
+
+export default connect(mapStateToProps)(UserProfile)
