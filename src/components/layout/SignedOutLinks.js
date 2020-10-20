dcc4eeff5275379ab1726687cd6e25b74b672d9f
rodrigo <rodrigomp88@gmail.com>
2020-10-20 13:43:33 -0300

    Ingresar

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -7,7 +7,7 @@ const SignedOutLinks = () => {
     <div>
       <ul className="right container">
         <ul><NavLink to='/crearusuario'>Registrarse</NavLink></ul>
-        <ul><NavLink to='/entrar'>Login</NavLink></ul>
+        <ul><NavLink to='/entrar'>Ingresar</NavLink></ul>
       </ul>
     </div>
   )
