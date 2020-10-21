17fa4a69b5f086a9533853aa825e75cea511fcf3
Acarvajal905 <acarvajal905@gmail.com>
2020-10-21 03:32:32 -0300

    agregado el 2do carrusel y otras cosas

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Carrousel } from "../Carrousel/Carrousel";
+import { Carrousel } from "../Carrousel/Carrousel"
+import { Carrousel2 } from "../Carrousel/Carrousel2";
 import { EventsHome } from "../Events/EventsHome";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
@@ -10,15 +11,16 @@ import "./Home.css";
 export const Home = () => {
   return (
     <div className="StyleHome">
-      <Carrousel />
-
       <div>
-        <div className="container">
+        <div >
+
+          <Carrousel2 />
           <div className="row">
             <div
               className="col-md-6 white-text text-center text-md-left mt-xl-5 mb-5 wow fadeInLeft"
               data-wow-delay="0.3s"
             >
+
               <h1 className="h1-responsive font-weight-bold mt-sm-5">
                 PROTECTORA DE ANIMALES DOMINGO FAUSTINO SARMIENTO{" "}
               </h1>
@@ -33,8 +35,10 @@ export const Home = () => {
               src="/img/Gallery/Dogs/Dogo4.jpg"
               alt=""
             />
+            <Carrousel />
           </div>
         </div>
+
       </div>
       <EventsHome />
     </div>
