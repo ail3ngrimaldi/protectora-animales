3f460960616dbddf8f9ec95ce678961d5a84e5b4
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2020-10-23 02:44:04 -0300

    Intentando traer usuarios a tabla

--- src/components/Administrator/Tablas/UsuariosAdmin.js
+++ src/components/Administrator/Tablas/UsuariosAdmin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,25 +8,32 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+import { db } from "../../../index";
+
+const ListUsers = () => {
+  const [links, setLinks] = useState([]);
+  const [currentId, setCurrentId] = useState("");
+
+  const getLinks = async () => {
+    db.collection("users").onSnapshot((querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setLinks(docs);
+    });
+  };
+
+  useEffect(() => {
+    getLinks();
+  }, []);
+
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function BasicTable() {
   const classes = useStyles();
 
   return (
@@ -38,19 +45,19 @@ export default function BasicTable() {
             <TableCell >Apellido</TableCell>
             <TableCell >Email</TableCell>
             <TableCell >Celular</TableCell>
-            <TableCell >Solicitudes</TableCell>
+            <TableCell >Acciones</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
+          {links.map((user) => (
+            <TableRow key={user.id}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {user.name}
               </TableCell>
-              <TableCell >{row.calories}</TableCell>
-              <TableCell >{row.fat}</TableCell>
-              <TableCell >{row.carbs}</TableCell>
-              <TableCell>{row.protein}</TableCell>
+              <TableCell component="th" scope="row" >{user.age}</TableCell>
+              <TableCell component="th" scope="row" >{user.mail}</TableCell>
+              <TableCell component="th" scope="row" >{user.name}</TableCell>
+              <TableCell component="th" scope="row" >{user.name}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -58,3 +65,5 @@ export default function BasicTable() {
     </TableContainer>
   );
 }
+
+export default ListUsers;
