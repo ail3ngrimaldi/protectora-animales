933704e9994a826c83bc977db9e9c5eb30ede97d
admin <admin@MacBook-Pro.fibertel.com.ar>
2020-10-28 20:32:35 -0300

    Donaciones

--- src/components/Donations/MoneyBanner.js
+++ src/components/Donations/MoneyBanner.js
@@ -13,143 +13,50 @@ import { Container } from "reactstrap";
 import "./MoneyBanner.css";
 import Castracion from '../Donations/imgD/Castracion.jpg';
 import MercadoPago from '../Donations/imgD/MercadoPago.jpg';
+import Insumos from '../Donations/imgD/Insumos.jpg';
 
 
 export const MoneyBanner = () => {
     return (
+      <div class="MBannerContainer w-75 pt-5 pb-3 mx-auto ">
+        <div className='center-block'>
+          <div class="hovereffect">
+              <img 
+              class="img-responsive w-100" 
+              src={Castracion} 
+              alt=""
+              />
+              <div class="overlay">
+                {/* <h2>Hover effect 1v2</h2> */}
+                <a class="info" href="/MoneyDonations">Doná una castración</a>
+              </div>
+          </div>
 
-      <div>
-        <a href="/MoneyDonations">
-        <img 
-        src={Castracion}
-        alt='castracion'
-        className='w-75 pt-5 pb-3 mx-auto d-block'
-        />
-        </a>
-        <a href="/MoneyDonations">
-        <img 
-        src={MercadoPago}
-        alt='mercadoPago'
-        className='w-75 pt-3 mx-auto d-block'
-        />
-        </a>
-        
+          <div class="hovereffect mt-5 mb-5">
+              <img 
+              class="img-responsive w-100" 
+              src={Insumos} 
+              alt=""
+              />
+              <div class="overlay">
+                {/* <h2>Hover effect 1v2</h2> */}
+                <a class="info" href="/MoneyDonations">Doná insumos </a>
+              </div>
+          </div>
+
+          <div class="hovereffect">
+              <img 
+              class="img-responsive w-100" 
+              src={MercadoPago} 
+              alt=""
+              />
+              <div class="overlay">
+                {/* <h2>Hover effect 1v2</h2> */}
+                <a class="info" href="/MoneyDonations">Doná por Mercado Pago</a>
+              </div>
+          </div>
       </div>
-      //   <div>
-      //       <div className="MBannerContainer">
-      //           {/* <img
-      //                   class="Mbannerimg"
-      //                   src="img/55853786_620357515076511_739317193384132608_n.jpg"
-      //                   alt="Card cap"
-                        
-      //           ></img> */}
-      //           <Container fluid className="StyleCarousel">
-      //   <div
-      //     id="carouselExampleCaptions"
-      //     className="carousel slide"
-      //     data-ride="carousel"
-      //   >
-      //     <ol className="carousel-indicators">
-      //       <li
-      //         data-target="#carouselExampleCaptions"
-      //         data-slide-to="0"
-      //         className="active"
-      //       ></li>
-      //       <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-      //       <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-      //       <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-      //     </ol>
-      //     <div className="carousel-inner">
-      //       <div className="carousel-item active">
-      //         <img src={Foto1} className="d-block w-100" alt="..." />
-      //         <div className="carousel-caption d-none d-md-block">
-                
-      //           <p>Protectora Sarmiento</p>
-      //         </div>
-      //       </div>
-      //       <div className="carousel-item">
-      //         <img src={Foto2} className="d-block w-100" alt="..." />
-      //         <div className="carousel-caption d-none d-md-block">
-                
-      //           <p>Protectora Sarmiento</p>
-      //         </div>
-      //       </div>
-      //       <div className="carousel-item">
-      //         <img src={Foto3} className="d-block w-100" alt="..." />
-      //         <div className="carousel-caption d-none d-md-block">
-                
-      //           <p>Protectora Sarmiento</p>
-      //         </div>
-              
-      //       </div>
-      //       <div className="carousel-item">
-      //         <img src={Foto4} className="d-block w-100" alt="..." />
-      //         <div className="carousel-caption d-none d-md-block">
-                
-      //           <p>Protectora Sarmiento</p>
-      //         </div>
-              
-      //       </div>
-      //       <div className="carousel-item">
-      //         <img src={Foto5} className="d-block w-100" alt="..." />
-      //         <div className="carousel-caption d-none d-md-block">
-                
-      //           <p>Protectora Sarmiento</p>
-      //         </div>
-              
-      //       </div>
-      //       <div className="carousel-item">
-      //         <img src={Foto6} className="d-block w-100" alt="..." />
-      //         <div className="carousel-caption d-none d-md-block">
-                
-      //           <p>Protectora Sarmiento</p>
-      //         </div>
-              
-      //       </div>
-      //     </div>
-      //     <a
-      //       className="carousel-control-prev"
-      //       href="#carouselExampleCaptions"
-      //       role="button"
-      //       data-slide="prev"
-      //     >
-      //       <span
-      //         className="carousel-control-prev-icon"
-      //         aria-hidden="true"
-      //       ></span>
-      //       <span className="sr-only">Previous</span>
-      //     </a>
-      //     <a
-      //       className="carousel-control-next"
-      //       href="#carouselExampleCaptions"
-      //       role="button"
-      //       data-slide="next"
-      //     >
-      //       <span
-      //         className="carousel-control-next-icon"
-      //         aria-hidden="true"
-      //       ></span>
-      //       <span className="sr-only">Next</span>
-      //     </a>
-      //   </div>
-      // </Container>
-      //           <div className="MBannerCard">
-      //               <h4 class="card-title">Donativo monetario</h4>
-      //               <h5 class="card-title">🐾 No nos suelten las patitas 🐾 </h5>
-      //               <h5 class="card-title">Los aportes económicos son importantes para pagar tratamientos, estudios médicos y honorarios veterinarios, comprar insumos y alimento, financiar 
-      //               castraciónes, imprimir material de difusión entre otros.</h5>
-      //               <h5 class="card-title">Agradecemos las colaboraciones para los Sarmientitos.</h5>
-      //               <h6 class="card-title">Necesitamos tu ayuda. Gracias.</h6>
-                 
-      //               <NavLink
-      //                   exact
-      //                   activeClassName="active"
-      //                   to="/MoneyDonations"
-      //                   className="nav-item nav-link">
-      //                   <button type="button" class="btn btn-primary">¡DONA AQUI!</button>
-      //               </NavLink>
-      //           </div>
-      //       </div>
-      //   </div>
+      </div>
+
     )
 }
--- src/components/Donations/MoneyDonations.js
+++ src/components/Donations/MoneyDonations.js
@@ -7,21 +7,24 @@ export const MoneyDonations = () => {
         <div>
 
             <div className="Container">
-                <div className="CBUCard">
-                    <h4 class="card-title">Depósito o transferencia Bancaria</h4>
-                    <h6 class="card-subtitle mb-2 text-muted">(Doná el importe que quieras directamente en nuestra cuenta.)</h6>
-                    <h5 class="card-title">Cuenta Corriente Especial Banco Municipal</h5>
-                    <h5 class="card-title">CBU 0650020701000000757761</h5>
-                    <h5 class="card-title">Cuenta: 757766</h5>
-                    <h5 class="card-title">Asociación Civil sin Fines de Lucro</h5>
-                    <h6 class="card-title">Personería Jurídica 0599 / 70</h6>
-                    <h6 class="card-title">Cuit 30-70152191-5</h6>
-                </div>
-                <div className="CBUCard">
-                    <h4 class="card-title">Donacion de Insumos</h4>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="CBUCard border border-warning ml-4">
+                <h4 class="card-header">Depósito o transferencia Bancaria</h4>
+                <div class="card-body">
+                    <p class="card-title lead">Cuenta Corriente Especial Banco Municipal</p>
+                    <p class="card-text text-muted">(Doná el importe que quieras directamente en nuestra cuenta.) </p>
+                    <p class="card-title">CBU 0650020701000000757761</p>
+                    <p class="card-title">Cuenta: 757766</p>
+                    <p class="card-title">Asociación Civil sin Fines de Lucro</p>
+                    <p class="card-title">Personería Jurídica 0599 / 70</p>
+                    <p class="card-title">Cuit 30-70152191-5</p>
                     
+                </div>
+            </div>
+            <div class="CBUCard border border-warning ml-4">
+                <h4 class="card-header">Donacion de Insumos</h4>
+                <div class="card-body">
+                    <p class="card-subtitle mb-2 text-muted">Card subtitle</p>
+                    <p class="card-text">Agradecemos las colaboraciones para los Sarmientitos.</p>
                     <NavLink
                         exact
                         activeClassName="active"
@@ -31,26 +34,23 @@ export const MoneyDonations = () => {
                     </NavLink>
                 </div>
             </div>
+            
+            </div>
 
             <div className="CardContainer">
                 <div className="MPCard">
                     <img
-                        class="card-img-top"
+                        class="card-img-top max-width: 5%"
                         src="/img/mercadopago.png"
                         alt="Card cap"
+                        
+                    
                     ></img>
                     <h5 class="card-title">Doná a través de MERCADOPAGO</h5>
                     <h6 class="card-subtitle mb-2 text-muted">(Todas las tarjetas de crédito y débito, Pago Fácil, Rapipago, Link, Banelco y otros.)</h6>
 
-                <div className="Opcard">
-                    <h5 class="card-title">Dona a través de MERCADOPAGO</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <a href ="https://mpago.la/2MNYQgd" target="_blank">  {/* LINK AL MERCADO PAGO DE LAUTARO */}
-                        <button type="button" 
-                            class="btn btn-primary">¡Haz tu donativo AQUI!
-                        </button>
-                    </a>
-                </div> 
+                
+            
 
                 <div className="Opcard">
                     <h5 class="card-title">Dona 100 $ARG</h5>
@@ -101,6 +101,7 @@ export const MoneyDonations = () => {
                         </button>
                     </a>
                 </div> 
+                
             </div>
 
                 
