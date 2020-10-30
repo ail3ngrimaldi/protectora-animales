2911b07ef8a58ac7fd1b81ce85501aba73b4dcb3
rodrigo <rodrigomp88@gmail.com>
2020-10-30 12:01:36 -0300

    nada

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,14 +13,16 @@ export const Home = (props) => {
       <div>
         <div >
           <Carrousel2 />
-          <section>
-            <div class="alert alert-success" role="alert">
-                <h4 class="alert-heading">¡Atención!</h4>
-                <p>El refugio Sarmiento es una ONG (Organización No Gubernamental).</p>
-                <p>La protectora no recibe animales hoy en día porque estamos trabajando al límite de nuestra capacidad. Los animales que eventualmente ingresan son los que están en extrema emergencia, para  que el refugio no sea un lugar de depósito...</p>
-                <hr></hr>
-                <p class="mb-0">El principal objetivo consiste en cuidar y asistir en la adopción para entonces reubicar a nuestros animales..</p>     
-            </div> 
+          <section>{
+            props.bool ? <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">¡Atención!</h4>
+            <p>El refugio Sarmiento es una ONG (Organización No Gubernamental).</p>
+            <p>La protectora no recibe animales hoy en día porque estamos trabajando al límite de nuestra capacidad. Los animales que eventualmente ingresan son los que están en extrema emergencia, para  que el refugio no sea un lugar de depósito...</p>
+            <hr></hr>
+            <p class="mb-0">El principal objetivo consiste en cuidar y asistir en la adopción para entonces reubicar a nuestros animales..</p>     
+        </div> : null
+            }
+            
           </section>
           <div className="row">
             <div className="aboutus" data-wow-delay="0.3s">
